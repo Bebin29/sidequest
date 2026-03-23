aa2fa8958aeae6a4030a4fac327935fbd421ad4a
ole1409 <ole1409@icloud.com>
2026-03-23 12:57:05 +0100

    Initial Commit

---
+++ Sidequest3/Sidequest3App.swift
@@ -0,0 +1,17 @@
+//
+//  Sidequest3App.swift
+//  Sidequest3
+//
+//  Created by ole on 23.03.26.
+//
+
+import SwiftUI
+
+@main
+struct Sidequest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sidequest3Tests/Sidequest3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Sidequest3Tests.swift
+//  Sidequest3Tests
+//
+//  Created by ole on 23.03.26.
+//
+
+import Testing
+@testable import Sidequest3
+
+struct Sidequest3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

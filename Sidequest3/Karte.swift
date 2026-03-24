5f94adc5c60c70c3ab68f65b3f90e44b76d424ff
oleEdeka <ole.boehm@edeka.de>
2026-03-24 11:21:11 +0100

    ui

--- Sidequest3/Karte.swift
+++ Sidequest3/Karte.swift
@@ -7,25 +7,59 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
+import Combine
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    private let manager = CLLocationManager()
+    
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 52.5200, longitude: 13.4050),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        
+        DispatchQueue.main.async {
+            self.region.center = location.coordinate
+        }
+    }
+}
 
 struct Karte: View {
     
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(
-                latitude: 52.5200,
-                longitude: 13.4050
-            ),
-            span: MKCoordinateSpan(
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05
-            )
-        )
-    )
-
+    @StateObject private var locationManager = LocationManager()
+    
+    let places = [
+        CLLocationCoordinate2D(latitude: 53.49987, longitude: 10.00258),
+        CLLocationCoordinate2D(latitude: 53.49987, longitude: 10.00268),
+        CLLocationCoordinate2D(latitude: 53.49987, longitude: 10.00278)
+    ]
+    
     var body: some View {
-        Map(position: $position)
-            .ignoresSafeArea()
+        Map(coordinateRegion: $locationManager.region,
+            showsUserLocation: true,
+            annotationItems: places) { place in
+            
+            MapMarker(coordinate: place)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+extension CLLocationCoordinate2D: Identifiable {
+    public var id: String {
+        "\(latitude)\(longitude)"
     }
 }
 

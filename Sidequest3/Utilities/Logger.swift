b38d5a0a79c4020b5454ef7f578b7ad345224993
Bebin29 <138676022+Bebin29@users.noreply.github.com>
2026-03-24 17:19:18 +0100

    Update CI workflow to run only tests for Sidequest3; adjust Logger.swift for SwiftLint compliance

--- Sidequest3/Utilities/Logger.swift
+++ Sidequest3/Utilities/Logger.swift
@@ -11,6 +11,7 @@ enum Log {
 
     static let general = os.Logger(subsystem: subsystem, category: "general")
     static let network = os.Logger(subsystem: subsystem, category: "network")
+    // swiftlint:disable:next identifier_name
     static let ui = os.Logger(subsystem: subsystem, category: "ui")
     static let data = os.Logger(subsystem: subsystem, category: "data")
 }
